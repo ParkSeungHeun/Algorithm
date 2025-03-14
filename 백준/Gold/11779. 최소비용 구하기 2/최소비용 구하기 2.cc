f5bf11a56a8aa174cba2c9c8a863192b0f5d013f
ParkSeungHeun <118339189+ParkSeungHeun@users.noreply.github.com>
2025-03-14 15:34:07 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 104 ms, Memory: 5884 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -1,95 +1,98 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#include <climits>
 
 using namespace std;
 
 struct Node {
-    int start;
-    int end;
-    int cost;
+	int start;
+	int end;
+	int cost;
 
-    bool operator<(const Node& right) const {
-        return cost > right.cost; // 최소 힙을 만들기 위해 반대로 비교
-    }
+	bool operator<(Node right) const {
+		return cost > right.cost;
+	}
 };
 
 struct Cost {
-    int cost;
-    int path;
+	int cost;
+	int path;
 };
 
 void Solution(vector<Node> map[], int start, int end);
 
 int main() {
-    int N, M;
-    cin >> N >> M;
+	
+	int N, M;
+	cin >> N >> M;
 
-    vector<Node> map[1001];
+	vector<Node> map[1001];
 
-    for (int i = 0; i < M; i++) {
-        int start, end, cost;
-        cin >> start >> end >> cost;
+	for (int i = 0; i < M; i++) {
+		int start, end, cost;
+		cin >> start >> end >> cost;
 
-        map[start].push_back({ start, end, cost });
-    }
+		map[start].push_back({ start, end, cost });
+	}
 
-    int start, end;
-    cin >> start >> end;
+	int start, end;
+	cin >> start >> end;
 
-    Solution(map, start, end);
+	Solution(map, start, end);
 
-    return 0;
+	return 0;
 }
 
+// 다익스트라인데, 결과 배열에 경로를 저장하는 
 void Solution(vector<Node> map[], int start, int end)
 {
-    Cost arr[1001];
+	Cost arr[1001];
 
-    // 배열 초기화 (INT_MAX 사용)
-    for (int i = 0; i <= 1000; i++) {
-        arr[i].cost = INT_MAX;
-        arr[i].path = -1;
-    }
+	for (int i = 0; i <= 1000; i++) {
+		arr[i].cost = 21e8;
+	}
 
-    priority_queue<Node> pq;
-    arr[start].cost = 0;
-    arr[start].path = start;
+	priority_queue<Node> pq;
+	arr[start].cost = 0;
+	arr[start].path = start;
 
-    pq.push({start, start, 0});
+	pq.push({ start, start, 0 });
 
-    while (!pq.empty())
-    {
-        Node cur = pq.top();
-        pq.pop();
+	while (!pq.empty())
+	{
+		Node cur = pq.top();
+		pq.pop();
 
-        // 최단 거리가 갱신되지 않은 경우 continue
-        if (arr[cur.start].cost < cur.cost) continue;
+		if (arr[cur.start].cost < cur.cost) continue;
 
-        for (const auto& next : map[cur.start]) {
-            int nextcost = next.cost + arr[cur.start].cost;
+		for (int i = 0; i < map[cur.start].size(); i++) {
+			// next에 next 좌표 정보와 코스트가 있음
+			Node next = map[cur.start][i];
+			
+			int nextcost = next.cost + arr[cur.start].cost;
 
-            if (arr[next.end].cost > nextcost) {
-                arr[next.end].cost = nextcost;
-                arr[next.end].path = cur.start;
-                pq.push({next.end, next.end, nextcost});
-            }
-        }
-    }
+			// 값이 nextcost가 더 크면 넘어가기
+			if (arr[next.end].cost <= nextcost) continue;
 
-    cout << arr[end].cost << '\n';
+			// 만약 더 값싸면 
+			arr[next.end].path = cur.start;
+			arr[next.end].cost = nextcost;
+			pq.push({ next.end, end, nextcost });
+		}
+	}
 
-    vector<int> result;
-    int result_path = end;
-    while (result_path != start) {
-        result.push_back(result_path);
-        result_path = arr[result_path].path;
-    }
-    result.push_back(start);
+	cout << arr[end].cost << '\n';
+	int result_path = end;
+	vector<int> result;
+	result.push_back(end);
 
-    cout << result.size() << '\n';
-    for (int i = result.size() - 1; i >= 0; i--) {
-        cout << result[i] << " ";
-    }
+	while (result_path != start) {
+		result.push_back(arr[result_path].path);
+		result_path = arr[result_path].path;
+	}
+
+	cout << result.size() << '\n';
+	for (int i = result.size() - 1; i >= 0; i--) {
+		cout << result[i] << " ";
+	}
 }
