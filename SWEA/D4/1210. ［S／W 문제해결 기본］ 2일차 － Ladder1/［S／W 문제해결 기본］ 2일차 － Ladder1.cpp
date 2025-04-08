b55584f349bdbfec7715ffa85c7fc5ef8ca94dba
ParkSeungHeun <118339189+ParkSeungHeun@users.noreply.github.com>
2025-04-08 17:17:11 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 17 ms, Memory: 5,836 KB -BaekjoonHub

---
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int Judge(int arr[][100], int y, int x);
+
+int main() {
+    int T = 10;
+
+    for (int test_case = 1; test_case <= T; test_case++)
+    {
+        int N;
+        cin >> N;
+
+        vector<int> index;
+
+        int arr[100][100] = { 0, };
+
+        for (int i = 0; i < 100; i++) {
+            int num;
+            cin >> num;
+
+            if (num == 1) {
+                index.push_back(i);
+            }
+
+            arr[0][i] = num;
+        }
+
+        for (int i = 1; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                cin >> arr[i][j];
+            }
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < index.size(); i++) {
+            int height = 0;
+            bool flag = false;
+
+            result = i;
+
+            int x = i; 
+
+            while (height < 99) {
+                int judge = Judge(arr, height, index[x]);
+
+                if (judge == 2) {
+                    // 오른 쪽 이동
+                    x++;
+                }
+                else if (judge == 1) {
+                    x--;
+                }
+
+                height++;
+            }
+
+            if (arr[height][index[x]] == 2) {
+                result = i;
+                break;
+            }
+
+        }
+
+        
+        cout << "#" << test_case << " " << index[result] << '\n';
+
+    }
+    return 0;
+}
+
+// straight : 0, left : 1, right : 2
+int Judge(int arr[][100], int y, int x)
+{
+    if (x + 1 < 100) {
+        if (arr[y][x + 1] == 1) {
+            return 2;
+        }
+    }
+
+    if (x - 1 >= 0) {
+        if (arr[y][x - 1] == 1) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
